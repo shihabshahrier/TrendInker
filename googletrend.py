691d07dee629cddbd484d4944e6e67be7bad01c9
shihabshahrier <shahrier13@yahoo.com>
2025-04-19 15:40:08 +0600

    link and filter parameters

---
+++ googletrend.py
@@ -0,0 +1,7 @@
+geo = "US"
+hours = 4
+category = 2
+status = "active"
+sort = "title"
+
+link = f"https://trends.google.com/trending?geo={geo}&hours={hours}&category={category}&status={status}&sort={sort}"
